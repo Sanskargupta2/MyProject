63a05bc0c8035956bd19773f407730210584544f
Sanskargupta2 <sanskargupta0209@gmail.com>
2022-06-06 10:00:14 +0530

    branch 'íssue1' first commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
 
 public class Main {
 
+	static int countDigits(int number) {
+		if(number>=1 && number<=99)
+			return 1;
+		else if(number>=10 && number <=99)
+			return 2;
+		else if(number>=100 && number<=999)
+			return 3;
+		else
+			return -1;
+	}
 	public static void main(String[] args) {
 		System.out.println("This is main class");
+		int x=741;
+		if(countDigits(x)!= -1)
+			System.out.println(countDigits(x)+" "+"digits");
+		else
+			System.out.println("Multiple digited number");
 		// TODO Auto-generated method stub
-
 	}
-
 }
